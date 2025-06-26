b0a42a579089978f082b783a17b9749a38b18c6d
NicolasC-1997 <nicolaseduardo1997@outlook.cl>
2025-06-26 00:58:56 -0400

    cambios base de datos

--- evaluacion_tres/settings.py
+++ evaluacion_tres/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'evaluacion_tres.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbevaluacion',
+        'USER': 'admin',
+        'PASSWORD': 'admindb123',
+        'HOST': 'endpoint_rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
